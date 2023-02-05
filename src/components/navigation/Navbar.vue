1f09a951c733467643ae5f96f374eb57d5ae5d26
chrismar303 <chrismar303@gmail.com>
2023-02-05 00:43:03 -0800

    [Feat] Navigation Menu
    The purpose of this MR is to define the navigation menu with 2 links:
    Home and Food Draw.
    * Placed inside `App.vue` so the `navbar` is accessible in every
    component

---
+++ src/components/navigation/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <header
+    class="absolute flex w-full items-center justify-between px-16 py-4 font-bold text-white"
+  >
+    <h2 class="text-2xl text-primary-color">Food Of Luck</h2>
+    <nav class="flex gap-8 text-lg">
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link :to="{ name: 'food-draw' }">Food Draw</router-link>
+    </nav>
+  </header>
+</template>
