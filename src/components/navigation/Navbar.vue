f0541739db82da1a570421eb84fb1f9a91dceddc
chrismar303 <chrismar303@gmail.com>
2023-02-05 01:06:10 -0800

    Added styles for active router-link

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -4,8 +4,14 @@
   >
     <h2 class="text-2xl text-primary-color">Food Of Luck</h2>
     <nav class="flex gap-8 text-lg">
-      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link :to="{ name: 'home' }">Home</router-link> |
       <router-link :to="{ name: 'food-draw' }">Food Draw</router-link>
     </nav>
   </header>
 </template>
+
+<style scoped>
+nav .router-link-active {
+  @apply text-primary-color;
+}
+</style>
