949cd081655bae031770edc6c5e1bfe86a0373b7
chrismar303 <chrismar303@gmail.com>
2023-02-05 13:17:11 -0800

    [Feat] Base Button and Searchbar
    The purpose of this MR is to create a reusable button and searchbar
    component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BaseButton from '@/components/input/BaseButton.vue'
 import router from '@/router'
 import '@/index.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.component('BaseButton', BaseButton)
+app.use(router).mount('#app')
