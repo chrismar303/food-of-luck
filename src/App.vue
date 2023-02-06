c31cef126473d5c482a207f65c0c8f1455f38484
chrismar303 <chrismar303@gmail.com>
2023-02-05 21:17:37 -0800

    [Feat] Navigation Menu (#3)
    * [Feat] Navigation Menu
    
    The purpose of this MR is to define the navigation menu with 2 links:
    Home and Food Draw.
    * Placed inside `App.vue` so the `navbar` is accessible in every
    component
    
    * Added styles for active router-link

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <navbar />
+    <router-view></router-view>
+  </div>
 </template>
+
+<script setup>
+import Navbar from '@/components/navigation/Navbar.vue'
+</script>
