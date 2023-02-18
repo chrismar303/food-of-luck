68c5ce7a181fb1161c73302566564c7afbc65dcc
chrismar303 <chrismar303@gmail.com>
2023-02-18 11:51:27 -0800

    Feat button and search components (#4)
    * [Feat] Navigation Menu
    
    The purpose of this MR is to define the navigation menu with 2 links:
    Home and Food Draw.
    * Placed inside `App.vue` so the `navbar` is accessible in every
    component
    
    * Added styles for active router-link
    
    * [Feat] Base Button and Searchbar
    
    The purpose of this MR is to create a reusable button and searchbar
    component
    
    * Include Searchbar code
    
    * [Feat] Implemented Home Page Design (#5)
    
    * [Feat] Implemented Home Page Design
    
    The purpose of this MR is to implement the HomePage design created in
    excalidraw
    
    * Added Background Photo

---
+++ src/components/input/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    :class="[
+      { 'rounded-l-xl': roundedLeft },
+      { 'rounded-r-xl': roundedRight },
+      'bg-primary-color py-2 px-4 text-lg font-bold text-white'
+    ]"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  roundedLeft: {
+    type: Boolean,
+    required: false,
+    default: true
+  },
+  roundedRight: {
+    type: Boolean,
+    required: false,
+    default: true
+  }
+})
+</script>
---
+++ src/components/input/Searchbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex">
+    <input
+      class="flex-1 rounded-l-xl px-4"
+      :type="type"
+      :name="name"
+      :placeholder="placeholder"
+    />
+    <base-button :rounded-left="false">GO</base-button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: false
+  },
+  name: {
+    type: String,
+    requred: false
+  },
+  placeholder: {
+    type: String,
+    required: false
+  }
+})
+</script>
