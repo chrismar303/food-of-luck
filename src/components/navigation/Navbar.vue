4574555e1e6dfa47446da7ba705455a4c9783fe9
chrismar303 <chrismar303@gmail.com>
2023-02-20 00:04:04 -0800

    Navbar Name Uppercase

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -2,7 +2,7 @@
   <header
     class="absolute flex w-full items-center justify-between px-16 py-4 font-bold text-white"
   >
-    <h2 class="text-2xl text-primary-color">Food Of Luck</h2>
+    <h2 class="text-2xl text-primary-color">FOOD OF LUCK</h2>
     <nav class="flex gap-8 text-lg">
       <router-link :to="{ name: 'home' }">Home</router-link> |
       <router-link :to="{ name: 'food-draw' }">Food Draw</router-link>
