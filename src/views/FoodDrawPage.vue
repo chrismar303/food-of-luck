373914d0ac99704c1dcdfb59519a75593dee1b66
chrismar303 <chrismar303@gmail.com>
2023-02-19 23:02:10 -0800

    [Feat] Food Draw: Basic Gameplay
    The purpose of this MR is to establish the basic functionality for
    playing the `Food Draw`. When the page is loaded, 20 locations are
    selected. There are 4 rounds. Each round eliminates half the options
    until 1 is left.

--- src/views/FoodDrawPage.vue
+++ src/views/FoodDrawPage.vue
@@ -1,3 +1,79 @@
 <template>
-  <h1 class="text-6xl">Food Draw</h1>
+  <section class="section pt-24">
+    <div class="w-3/4">
+      <div class="mb-4 flex items-center justify-between gap-12">
+        <h2 class="text-bold text-2xl text-white">
+          Total Contestants Remaining:
+          <span class="text-primary-color">
+            {{ state.draw.length }}
+          </span>
+        </h2>
+        <div class="flex gap-4">
+          <base-button @click="drawRestaurants"> Draw </base-button>
+          <base-button outline @click="state.draw = [...state.restaurants]">
+            Reset
+          </base-button>
+        </div>
+      </div>
+
+      <ul class="flex h-3/5 flex-wrap justify-evenly gap-4 overflow-y-auto">
+        <li
+          v-for="restaurant in state.draw"
+          :key="restaurant.id"
+          class="h-64 w-64"
+        >
+          <draw-card :restaurant="restaurant" />
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
+
+<script setup>
+import { reactive, inject } from 'vue'
+import { useRoute } from 'vue-router'
+import DrawCard from '@/components/draw/DrawCard.vue'
+
+const route = useRoute()
+const yelpApi = inject('yelpApi')
+const state = reactive({ restaurants: [], draw: [] })
+
+yelpApi
+  .get('/search', { params: { location: route.query.location } })
+  .then(({ data }) => {
+    state.restaurants = data.businesses
+    state.draw = [...state.restaurants]
+  })
+
+function drawRestaurants() {
+  // remove half of the items randomly
+  let i = 0
+  const itemsToRemove = state.draw.length / 2
+  while (i < itemsToRemove) {
+    const index = selectRandomIndex()
+    state.draw.splice(index, 1)
+    ++i
+  }
+}
+
+function selectRandomIndex() {
+  const max = state.draw.length - 1
+  return Math.floor(Math.random() * (max + 1))
+}
+</script>
+
+<style scoped>
+.section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  min-height: 100dvh;
+  background: linear-gradient(
+    112.1deg,
+    rgb(32, 38, 57) 11.4%,
+    rgb(63, 76, 119) 70.2%
+  );
+}
+</style>
