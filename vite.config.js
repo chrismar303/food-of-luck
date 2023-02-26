64caf550ba31dd1cb5b61f395ba119af357efc61
chrismar303 <chrismar303@gmail.com>
2023-02-26 00:44:39 -0800

    Remove forward slash

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/food-of-luck/',
+  base: '/food-of-luck',
   plugins: [vue()],
   resolve: {
     alias: {
